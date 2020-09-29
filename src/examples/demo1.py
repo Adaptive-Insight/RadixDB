0331961bee9692076c0b87b7f253b021cc587cc0
Min Wei <minwei@Mins-MacBook-Pro.local>
2020-09-28 21:39:31 -0700

    remove internal files

--- src/examples/demo1.py
+++
@@ -1,3 +0,0 @@
-import radixdb.evaluator
-c=radixdb.evaluator.eval("Q(T.hist_prices).fields(['high', 'low']).limit(10).show()")
-print(c)
--- src/examples/models/stub.py
+++
@@ -1,8 +0,0 @@
-import radixdb.executor
-import matplotlib
-matplotlib.use('webagg')
-import matplotlib.pyplot as plt
-
-df = radixdb.executor.df_query("select date::date, data from co2;", "postgres://postgres@localhost:5432/radixdb")
-exec(open("./co2_model.py").read(), {"_df": df})
-plt.savefig('co2.png')
